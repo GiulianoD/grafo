5baccace0dc7ab9ad7c168f2e7ad80dc23f4d57d
Giuliano de Paulo Demoner <giuliano.demoner@gmail.com>
2022-10-31 23:43:16 -0300

    finalizada estruture base do grafo

--- src/estrutura/Grafo.java
+++ src/estrutura/Grafo.java
@@ -1,12 +1,38 @@
 package estrutura;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class Grafo<TIPO> {
-    private ArrayList<TIPO> vertices;
-    private ArrayList<TIPO> arestas;
-    private List<Aresta> arestaLst;
-    private List<Vertice> verticeLst;
+    private ArrayList<Vertice<TIPO>> vertices;
+    private ArrayList<Aresta<TIPO>> arestas;
 
-    private Aresta predecessor;
+    public Grafo(ArrayList<Vertice<TIPO>> vertices, ArrayList<Aresta<TIPO>> arestas) {
+        this.vertices = vertices;
+        this.arestas = arestas;
+    }
+
+    public void addVertice(TIPO dado){
+        Vertice<TIPO> novoVertice = new Vertice<TIPO>(dado);
+        this.vertices.add(novoVertice);
+    }
+
+    public void addAresta(Double peso, TIPO dadoInicio, TIPO dadoFim){
+        Vertice<TIPO> inicio = this.getVertice(dadoInicio); // busca pelo vertice da ponta de inicio da aresta
+        Vertice<TIPO> fim    = this.getVertice(dadoFim); // busca pelo vertice do fim da aresta
+
+        Aresta<TIPO> novaAresta = new Aresta<TIPO>(peso, inicio, fim); // cria aresta com inicio e fim necessariamente definidos
+
+        // agora adiciona a informaacao do novo caminho entre os vertices que compoem suas pontas
+        inicio.addArestaSaida(novaAresta);
+        fim.addArestaEntrada(novaAresta);
+
+        // por fim, adiciona a aresta na lista de arestas do grafo
+        this.arestas.add(novaAresta);
+    }
+
+    public Vertice<TIPO> getVertice (TIPO dado){
+        for (Vertice<TIPO> v : vertices)
+            if (v.getDado() == dado) return v;
+        return null;
+    }
 }
--- src/estrutura/Vertice.java
+++ src/estrutura/Vertice.java
@@ -1,4 +1,29 @@
 package estrutura;
-public class Vertice {
-    
+
+import java.util.ArrayList;
+
+public class Vertice<TIPO> {
+    private TIPO dado;
+    private ArrayList<Aresta<TIPO>> arestasEntradas;
+    private ArrayList<Aresta<TIPO>> arestasSaidas;
+
+    public Vertice(TIPO dado){
+        this.dado = dado;
+        this.arestasEntradas = new ArrayList<Aresta<TIPO>>();
+        this.arestasSaidas = new ArrayList<Aresta<TIPO>>();
+    }
+
+    public TIPO getDado() {
+        return dado;
+    }
+    public void setDado(TIPO dado) {
+        this.dado = dado;
+    }
+
+    public void addArestaEntrada (Aresta<TIPO> aresta){
+        this.arestasEntradas.add(aresta);
+    }
+    public void addArestaSaida (Aresta<TIPO> aresta){
+        this.arestasSaidas.add(aresta);
+    }
 }
